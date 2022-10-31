57b514777ad6b598809e061c652ea90ee028949d
Thilina Lakshan <thilina.18@cse.mrt.ac.lk>
2022-10-31 19:03:41 +0530

    update installation script

--- install.sh
+++ install.sh
@@ -36,9 +36,38 @@ echo "Installing ZSH config..."
 
 # check zsh is installed
 if ! command -v zsh > /dev/null; then
-    echo "zsh is not installed"
+    echo "zsh is not installed."
     exit 1
 fi
 
+# check starship is installed
+if ! command -v starship > /dev/null; then
+    echo "starship is not installed, run following command to install it."
+    echo "curl -sS https://starship.rs/install.sh | sh"
+    exit 1
+fi
 
+# check git is installed
+if ! command -v git > /dev/null; then
+    echo "git is not installed."
+    exit 1
+fi
 
+# download config file 
+download_zsh_config
+
+# generate .zshrc 
+generate_zshrc
+
+# check zsh is default shell 
+if [ "$SHELL" != "$(command -v zsh)" ]; then
+    echo "zsh is not default shell."
+    echo "run 'chsh -s $(command -v zsh)' to set zsh as default shell."
+    printf "Do you want to set zsh as default shell? [y/N]: "
+    read -r answer
+    if [ "$answer" = "y" ] || [ "$answer" = "Y" ]; then
+        chsh -s "$(command -v zsh)"
+    fi
+fi
+
+echo "Done."
